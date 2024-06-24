ff480484813d703287c053126125cc68965ec8d5
NonzeroCornet34 <ullerbri000@gmail.com>
2024-06-23 21:15:46 -0600

    Fixed security issues

--- index.js
+++ index.js
@@ -8,6 +8,58 @@ const db = new sqlite3.Database("./users.db");
 const app = express();
 const port = 3001;
 
+const achievements = [
+  "🚀 Activate the ID in front of another ship on accident",
+  "🕐 Finish a 5 hour without the crew destroying a single ship",
+  "⏱️ Finish briefing on under 10 minutes",
+  "📛 Have each crew member consistently answer to a code name",
+  "🏕️ Get a camp crew of entirely single riders (No one came together to the camp)",
+  "🤥 Tell a crew that they are a liar and a poor one at that but by a protagonist.",
+  "🎵 End your mission at the natural end of your playlist.",
+  "🎶 Get a really good unplanned music que.",
+  "🔧 Fix a problem in the ship using a trick Jon taught you.",
+  "🔥 Crew member comments on a savage burn to them or another character",
+  "👋 Crew member recognizes you from a previous flight",
+  "🎙️ Go an entire flight without using the wrong voice setting on the voice changer.",
+  "🔊 Use every setting on the voice changer in one flight.",
+  "🏃 Lose mic cable mid flight and sprint to obtain and install a new one.",
+  "🎭 Make a reference to another character not involved in the mission in character.",
+  "😂 Laugh so loud the crew comments on it from the bridge.",
+  "👽 Have a crew try and speak Klingon to a Klingon",
+  "💕 Crew member flirts with a character",
+  "💡 You and your staff are so still for long enough the lights turn off.",
+  "🍔 Eat from both Wendy's and Culver's in one day.",
+  "🗨️ Have a crew member quoting something you said as they leave because they found it hilarious.",
+  "🎂 Crew sings happy birthday on the bridge to another crew member or character.",
+  "❤️ Your crew tells Apollo they love them.",
+  "⚫ Finish a full camp without blacking out for a strike/death once.",
+  "👻 Someone other than yourself invokes the oven ghost.",
+  "😱 You or a crew member is spooked by a loud Ellie or Scott.",
+  "💀 Crew threatens to kill Chuck or Chad.",
+  "😆 Make your supervisor laugh out loud on the bridge.",
+  "🎯 Have a failed bride retake or take or other phaser scenario they need to win.",
+  '🤣 Crew member makes a "your mom" joke to a villain.',
+  "🖥️ Laugh as the computer accidentally.",
+  "📺 Make a reference as a character to a tv show or movie.",
+  "🩸 Forget you are wearing blood makeup.",
+  "🧁 Make the two muffin joke as the computer.",
+  '🤥 Find a way to say "you are such a liar and a poor one at that" as someone other than the Ferengi.',
+  "🏢 Mention something about the old building.",
+  "🍪 Eat one of the moist pumpkin cookies Vic brings.",
+  "💧 Drink at least two water bottles full of water.",
+  "☕ Drink hot cocoa or tea in the odyssey.",
+  "😴 Take a nap in the Cassini bunks.",
+  "💤 Take a nap in any of the control rooms.",
+  "🛠️ Have a crew member with the same name as your engineer.",
+  "☎️ Have a three way call with Sontall, Wilkes, and the cap.",
+  "🎉 Plan a surprise away mission.",
+  "🎯 Call shots as a character other than the doctor (or doctor replacement) and not staff.",
+  "💡 Fix the lighting when it isn't working.",
+  "💥 Play explosions instead of computer beeps.",
+  "🚢 Crew member invokes the full name of their ship including registry number when introducing themselves.",
+  "⚔️ Crew member promises to avenge someone.",
+];
+
 var users = [];
 
 function getUsersWithAchievements(callback) {
@@ -154,20 +206,36 @@ app.post("/new", (req, res) => {
 });
 
 app.post("/grant", (req, res) => {
-  try {
-    for (let i = 0; i < users.length; i++) {
-      const user = users[i];
-      if (user.id != req.body.id && user.password == req.body.password) {
-        grantAchievement(req.body.id, req.body.achievement);
-        res.sendStatus(200);
-        break;
-      } else if (i == users.length - 1) {
+  if (req.body.achievement) {
+    if (
+      achievements.indexOf(
+        req.body.achievement.name + req.body.achievement.description,
+      ) != -1
+    ) {
+      try {
+        for (let i = 0; i < users.length; i++) {
+          const user = users[i];
+          if (user.id != req.body.id && user.password == req.body.password) {
+            if (
+              !user.achievements.some(
+                (obj) => obj.description === req.body.achievement.description,
+              )
+            ) {
+              grantAchievement(req.body.id, req.body.achievement);
+            }
+            res.sendStatus(200);
+            break;
+          } else if (i == users.length - 1) {
+            res.sendStatus(403);
+          }
+        }
+      } catch (e) {
+        console.log(e);
         res.sendStatus(403);
       }
+    } else {
+      res.sendStatus(403);
     }
-  } catch (e) {
-    console.log(e);
-    res.sendStatus(403);
   }
 });
 
